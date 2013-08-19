2a6dac12e3a53863f816d4cfd6a2b0b028f352fa
Peter Hoeg <peter@speartail.com>
2013-08-19 10:53:35 +0800

    clean up puppet-lint warnings

--- manifests/fetch.pp
+++ manifests/fetch.pp
@@ -1,21 +1,21 @@
 define fetchfact::fetch ( $url, $factfile ) {
 
   include fetchfact
-  $path = "/etc/facter/facts.d/$factfile"
+  $path = "/etc/facter/facts.d/${factfile}"
 
   case $factfile {
     /.*\.json$/: {
       case $osfamily {
         'redhat','debian','suse': {
-           package { "ruby-json":
-             ensure => installed
-           }
+          package { 'ruby-json':
+            ensure => installed
+          }
         }
         default: {
-           package { "json":
-             ensure => installed,
-             provider => gem
-           }
+          package { 'json':
+            ensure   => installed,
+            provider => gem
+          }
         }
       }
     }
@@ -25,11 +25,10 @@ define fetchfact::fetch ( $url, $factfile ) {
     }
   }
 
-
-  wget::fetch { "$url":
-    source => $url,
+  wget::fetch { $url:
+    source      => $url,
     destination => $path,
-    require => File['/etc/facter/facts.d']
+    require     => File['/etc/facter/facts.d']
   }
 
-}  
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,17 @@
 class fetchfact {
 
-  file { "/etc/facter":
+  file { '/etc/facter':
     ensure => directory,
-    owner => root,
-    group => root,
-    mode => 0755
+    owner  => root,
+    group  => root,
+    mode   => '0755'
   }
 
-  file { "/etc/facter/facts.d":
-    ensure => directory,
-    owner => root,
-    group => root,
-    mode => 0755,
-    require => File["/etc/facter"]
+  file { '/etc/facter/facts.d':
+    ensure  => directory,
+    owner   => root,
+    group   => root,
+    mode    => '0755',
+    require => File['/etc/facter']
   }
 }
